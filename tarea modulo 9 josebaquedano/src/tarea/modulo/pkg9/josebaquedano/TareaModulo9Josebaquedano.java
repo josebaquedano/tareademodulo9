8fb06ebb34fe7300f9adc3ecff7c8dbab85046db
josebaquedano <57683023+josebaquedano@users.noreply.github.com>
2019-12-06 19:11:16 -0600

    PROYECTO FINAL

---
+++ tarea modulo 9 josebaquedano/src/tarea/modulo/pkg9/josebaquedano/TareaModulo9Josebaquedano.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.modulo.pkg9.josebaquedano;
+
+/**
+ *
+ * @author WILLMER BAQUEDANO
+ */
+public class TareaModulo9Josebaquedano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         modulo9 p = new  modulo9();
+        p.setVisible(true);
+    }
+    
+}
